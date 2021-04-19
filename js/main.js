8c95826d28e9863242f1e7f03987a3becd85cb59
meyou9797 <80095780+meyou9797@users.noreply.github.com>
2021-04-19 16:55:24 +0900

    이디야 1차

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//슬릭구현
+$(document).ready(function(){
+    $('.mainSlider').slick({
+       prevArrow:'<div class="prevBtn">이전</div>',
+       nextArrow:'<div class="nextBtn">다음</div>',
+       });
+})
